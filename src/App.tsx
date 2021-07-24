1a28828c07d114c33cc03ae30da958fd71a1a9ad
Gabriel Souto <gabrielluizferraz@gmail.com>
2021-07-24 17:10:29 -0300

    added github deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import QuestionCard from './components/QuestionCard';
 import { fetchQuizQuestions, Difficulty, QuestionState } from './api';
+import { GlobalStyle } from './App.styles';
 
-type AnswerObject = {
+export type AnswerObject = {
   question: string;
   answer: string;
   correct: boolean;
@@ -18,7 +19,7 @@ const App = () => {
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
-
+  
   const startTrivia = async () => {
     setLoading(true);
     setGameOver(false);
@@ -66,27 +67,35 @@ const App = () => {
   }
 
   return (
-    <div className="App">
-      <h1> React Quizz</h1>
-      { gameOver || userAnswers.length === TOTAL_QUESTIONS ? 
-        <button className="start" onClick={startTrivia}>Start</button>
-      : null}
-      { !gameOver ? <p className="score">Score: {score}</p> : null }
-      { loading && <p>Loading Questions...</p> }
-      { !loading && !gameOver && 
-        <QuestionCard
-          questionNumber={number+1}
-          totalQuestions={TOTAL_QUESTIONS}
-          question={questions[number].question}
-          answers={questions[number].answers}
-          userAnswer={userAnswers ? userAnswers[number] : undefined}
-          callback={checkAnswer}
-        /> }
-      { (!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS -1) ? 
-        <button className="next" onClick={nextQuestion}>Next Question</button>
-        : null
-      }
-    </div>
+    <>
+      <GlobalStyle />
+        <div className="App">
+          <h1> Ash Quizz</h1>
+          { (gameOver || userAnswers.length === TOTAL_QUESTIONS) ? 
+            <button className="start" onClick={startTrivia}>Start</button>
+          : null}
+          {/* { (gameOver || userAnswers.length === TOTAL_QUESTIONS) && selectedDifficulty ? 
+            ['easy', 'medium', 'hard'].map(difficulty => {
+              return <button className="difficulty" value={difficulty} onClick={() => setSelectedDifficulty(difficulty)}>{difficulty && difficulty.toUpperCase}</button>
+            })
+          : null} */}
+          { !gameOver ? <p className="score">Score: {score}</p> : null }
+          { loading && <p>Loading Questions...</p> }
+          { !loading && !gameOver &&
+            <QuestionCard
+              questionNumber={number+1}
+              totalQuestions={TOTAL_QUESTIONS}
+              question={questions[number].question}
+              answers={questions[number].answers}
+              userAnswer={userAnswers ? userAnswers[number] : undefined}
+              callback={checkAnswer}
+            /> }
+          { (!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS -1) ? 
+            <button className="next" onClick={nextQuestion}>Next Question</button>
+            : null
+          }
+        </div>
+    </>
   );
 }
 
